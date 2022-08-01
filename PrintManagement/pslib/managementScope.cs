3a91c5292a7092961dbde9d6bde92fcf977ff405
Lyas Spiehler <lyas.spiehler@sapphirehealth.org>
2022-08-01 07:27:09 -0500

    created a singleton class for ManagementScope

---
+++ PrintManagement/pslib/managementScope.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintManagement.pslib
+{
+    public sealed class managementScope
+    {
+        private ManagementScope ms;
+        private static readonly managementScope instance = new managementScope();
+        static managementScope()
+        {
+        }
+        private managementScope()
+        {
+        }
+        public static managementScope Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        public ManagementScope Get()
+        {
+            if(ms != null && ms.IsConnected)
+            {
+                return ms;
+            }
+            var wmiConnectionOptions = new ConnectionOptions();
+            wmiConnectionOptions.Impersonation = ImpersonationLevel.Impersonate;
+            wmiConnectionOptions.Authentication = AuthenticationLevel.Default;
+            wmiConnectionOptions.EnablePrivileges = true; // required to load/install the driver.
+                                                          // Supposed equivalent to VBScript objWMIService.Security_.Privileges.AddAsString "SeLoadDriverPrivilege", True 
+
+            string path = "root\\cimv2";
+            ms = new ManagementScope(path, wmiConnectionOptions);
+            ms.Connect();
+
+            return ms;
+        }
+    }
+}
