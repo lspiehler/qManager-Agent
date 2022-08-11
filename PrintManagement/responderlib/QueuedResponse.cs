a925424f8106a0d25e0fa4a4633f7e2ae7840053
Lyas Spiehler <lspiehler@gmail.com>
2022-08-11 06:17:04 -0500

    remove caching from queued response so that it is handled entirely by the printQueue.GetAll method

--- PrintManagement/responderlib/QueuedResponse.cs
+++ PrintManagement/responderlib/QueuedResponse.cs
@@ -35,7 +35,7 @@ namespace PrintManagement.responderlib
         {
             //public string path { get; set; }
             public Dictionary<string, System.Net.WebSockets.Managed.ClientWebSocket> clients { get; set; }
-            public HashTableResponseBody cachedresponse { get; set; }
+            //public HashTableResponseBody cachedresponse { get; set; }
         }
 
         public async Task ProcessResponse(System.Net.WebSockets.Managed.ClientWebSocket ws, dynamic rm)
@@ -54,11 +54,11 @@ namespace PrintManagement.responderlib
                 //rlc.clients = new List<System.Net.WebSockets.Managed.ClientWebSocket>();
                 //rlc.clients.Add(rm.id, ws);
                 rlc.clients = new Dictionary<string, System.Net.WebSockets.Managed.ClientWebSocket>();
-                rlc.cachedresponse = null;
+                //rlc.cachedresponse = null;
                 cachedtypes.Add(path, rlc);
             }
 
-            if (cachedtypes[path].cachedresponse != null && updatecache != true)
+            /*if (cachedtypes[path].cachedresponse != null && updatecache != true)
             {
                 HashTableResponse resp = new HashTableResponse();
 
@@ -73,21 +73,6 @@ namespace PrintManagement.responderlib
                     );
 
                 Console.WriteLine("Sending cached response");
-                /*try {
-                    await ws.SendAsync(
-                        bytestosend,
-                        WebSocketMessageType.Text,
-                        true,
-                        CancellationToken.None
-                    );
-
-                    //ws.Dispose();
-                }
-                catch (Exception e)
-                {
-                    errorlog el = new errorlog();
-                    el.write(e.ToString(), Environment.StackTrace, "error");
-                }*/
                 try
                 {
                     await wsresponser.Send(ws, bytestosend);
@@ -99,103 +84,103 @@ namespace PrintManagement.responderlib
                 }
             }
             else
+            {*/
+            if (cachedtypes[path].clients.Count > 0)
             {
-                if (cachedtypes[path].clients.Count > 0)
+                // just add pending request to the queue
+                Console.WriteLine("Adding pending request to the queue");
+                cachedtypes[path].clients.Add(rm.id, ws);
+            }
+            else
+            {
+                cachedtypes[path].clients.Add(rm.id, ws);
+
+                HashTableResponseBody body = new HashTableResponseBody();
+                if (path == "/printer/queue/list")
                 {
-                    // just add pending request to the queue
-                    Console.WriteLine("Adding pending request to the queue");
-                    cachedtypes[path].clients.Add(rm.id, ws);
+                    //PMPrintQueues pmprintqueues = new PMPrintQueues();
+
+                    //Hashtable myPrintQueues = pmprintqueues.getQueues();
+                    //pslib.getPrinter getprinter = new pslib.getPrinter();
+
+                    body.result = "success";
+                    body.message = null;
+                    body.data = new Hashtable(printqueue.GetAll(updatecache));
+                }
+                else if (path == "/printer/port/list")
+                {
+                    //pslib.getPrinterPort getprinterport = new pslib.getPrinterPort();
+
+                    body.result = "success";
+                    body.message = null;
+                    body.data = new Hashtable(printport.GetAll(updatecache));
+                }
+                else if (path == "/printer/driver/list")
+                {
+                    body.result = "success";
+                    body.message = null;
+                    body.data = new Hashtable(printdriver.Get(updatecache));
                 }
                 else
                 {
-                    cachedtypes[path].clients.Add(rm.id, ws);
 
-                    HashTableResponseBody body = new HashTableResponseBody();
-                    if (path == "/printer/queue/list")
+                }
+
+                Console.WriteLine("Responding to all requests");
+
+                List<string> clientkeys = new List<string>(cachedtypes[path].clients.Keys);
+
+                for (int i = cachedtypes[path].clients.Count - 1; i >= 0; i--)
+                {
+
+                    HashTableResponse resp = new HashTableResponse();
+
+                    //cachedtypes[path].cachedresponse = body;
+
+                    resp.id = clientkeys[i];
+                    resp.type = "response";
+                    resp.body = body;
+
+                    string jsonresp = JsonConvert.SerializeObject(resp, Formatting.Indented);
+
+                    //Console.WriteLine(jsonresp);
+
+                    ArraySegment<byte> bytestosend = new ArraySegment<byte>(
+                        Encoding.UTF8.GetBytes(jsonresp)
+                    );
+
+                    //Console.WriteLine("Sending response to message id " + clientkeys[i]);
+                    /*try
                     {
-                        //PMPrintQueues pmprintqueues = new PMPrintQueues();
-
-                        //Hashtable myPrintQueues = pmprintqueues.getQueues();
-                        //pslib.getPrinter getprinter = new pslib.getPrinter();
-
-                        body.result = "success";
-                        body.message = null;
-                        body.data = new Hashtable(printqueue.GetAll(updatecache));
-                    }
-                    else if (path == "/printer/port/list")
-                    {
-                        //pslib.getPrinterPort getprinterport = new pslib.getPrinterPort();
-
-                        body.result = "success";
-                        body.message = null;
-                        body.data = new Hashtable(printport.GetAll(updatecache));
-                    }
-                    else if (path == "/printer/driver/list")
-                    {
-                        body.result = "success";
-                        body.message = null;
-                        body.data = new Hashtable(printdriver.Get(updatecache));
-                    }
-                    else
-                    {
-
-                    }
-
-                    Console.WriteLine("Responding to all requests");
-
-                    List<string> clientkeys = new List<string>(cachedtypes[path].clients.Keys);
-
-                    for (int i = cachedtypes[path].clients.Count - 1; i >= 0; i--)
-                    {
-
-                        HashTableResponse resp = new HashTableResponse();
-
-                        cachedtypes[path].cachedresponse = body;
-
-                        resp.id = clientkeys[i];
-                        resp.type = "response";
-                        resp.body = body;
-
-                        string jsonresp = JsonConvert.SerializeObject(resp, Formatting.Indented);
-
-                        //Console.WriteLine(jsonresp);
-
-                        ArraySegment<byte> bytestosend = new ArraySegment<byte>(
-                            Encoding.UTF8.GetBytes(jsonresp)
+                        await cachedtypes[path].clients[clientkeys[i]].SendAsync(
+                            bytestosend,
+                            WebSocketMessageType.Text,
+                            true,
+                            CancellationToken.None
                         );
 
-                        //Console.WriteLine("Sending response to message id " + clientkeys[i]);
-                        /*try
-                        {
-                            await cachedtypes[path].clients[clientkeys[i]].SendAsync(
-                                bytestosend,
-                                WebSocketMessageType.Text,
-                                true,
-                                CancellationToken.None
-                            );
-
-                            //cachedtypes[path].clients[clientkeys[i]].Dispose();
-                        }
-                        catch(Exception e)
-                        {
-                            errorlog el = new errorlog();
-                            el.write(e.ToString(), Environment.StackTrace, "error");
-                        }*/
-                        try
-                        {
-                            await wsresponser.Send(cachedtypes[path].clients[clientkeys[i]], bytestosend);
-                        }
-                        catch (Exception e)
-                        {
-                            errorlog el = new errorlog();
-                            el.write(e.ToString(), Environment.StackTrace, "error");
-                        }
-                        cachedtypes[path].clients.Remove(clientkeys[i]);
+                        //cachedtypes[path].clients[clientkeys[i]].Dispose();
                     }
-
-                    Console.WriteLine(cachedtypes[path].clients.Count + " remaining");
+                    catch(Exception e)
+                    {
+                        errorlog el = new errorlog();
+                        el.write(e.ToString(), Environment.StackTrace, "error");
+                    }*/
+                    try
+                    {
+                        await wsresponser.Send(cachedtypes[path].clients[clientkeys[i]], bytestosend);
+                    }
+                    catch (Exception e)
+                    {
+                        errorlog el = new errorlog();
+                        el.write(e.ToString(), Environment.StackTrace, "error");
+                    }
+                    cachedtypes[path].clients.Remove(clientkeys[i]);
                 }
+
+                Console.WriteLine(cachedtypes[path].clients.Count + " remaining");
             }
+            //}
         }
     }
 }
