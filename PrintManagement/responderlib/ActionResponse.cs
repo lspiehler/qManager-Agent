ee7d5aff75d7153789eb1841de2c583944429ac8
Lyas Spiehler <lspiehler@gmail.com>
2026-02-24 17:13:54 -0600

    reapply updated code (starttime and untiltime) with older Newtonsoft.Json

--- PrintManagement/responderlib/ActionResponse.cs
+++ PrintManagement/responderlib/ActionResponse.cs
@@ -433,7 +433,7 @@ namespace PrintManagement.responderlib
                     body.data = new Hashtable()
                     {
                         {"hostname", GetLocalhostFqdn()},
-                        {"agentVersion", "0.104" },
+                        {"agentVersion", "0.105" },
                         {"groups", groups}
                     };
                 }
